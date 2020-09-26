17cfef860634403693c4044c6f856e8c24b8689d
Lungelo Masondo <lsxmasondo@gmail.com>
2020-09-26 21:18:08 +0200

    Updates

---
+++ precache-manifest.7a2f2eb60d76b37f63389d3c0c0e0519.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c46b33ca4e91a9fcdbe8d9d5eb6a0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73b37b7834fd1b9334c1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "07fd3b11abe0a9f883f2",
+    "url": "/static/js/2.8b8fd828.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b8fd828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b37b7834fd1b9334c1",
+    "url": "/static/js/main.c9653357.chunk.js"
+  },
+  {
+    "revision": "a43230c07d0dfc6119af",
+    "url": "/static/js/runtime-main.27715456.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
